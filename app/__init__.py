e028d03c1274a4425b9e7bca35e9038b1640175d
koderny <haroldpedraza@gmail.com>
2025-04-18 18:51:16 -0400

    calendar working

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask
+# IMPORTS
+from flask import Flask, render_template
 import os
 from app import routes
 
+# SETUP
 app = Flask(__name__)
 app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
 app.register_blueprint(routes.bp)
