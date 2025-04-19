3b2b6ab5441c8f5cc8af3ec56ae0de1f312a4604
koderny <haroldpedraza@gmail.com>
2025-04-18 21:50:48 -0400

    table renders but order is incorrect

--- app/routes.py
+++ app/routes.py
@@ -8,18 +8,27 @@ DB_FILE = os.environ.get("DB_FILE")
 
 @bp.route("/")
 def main():
-    return render_template("main.html")
+    with sqlite3.connect(DB_FILE) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT id, name, start_datetime, end_datetime, description, private
+            FROM appointments
+            ORDER BY start_datetime;
+        """)
+        rows = cursor.fetchall()
 
+        appointments = []
+        for row in rows:
+            appointments.append({
+            # "id": row[0],
+            "name": row[1],
+            "start": datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S'),
+            "end": datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S'),
+            # "description": row[4],
+            # "private": bool(row[5])
+        })
+    return render_template("main.html", rows=rows, appointments=appointments)
 
-    # appointments = []
-    # for row in rows:
-    #     appointments.append({
-    #         "id": row[0],
-    #         "name": row[1],
-    #         "start": datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S'),
-    #         "end": datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S'),
-    #         "description": row[4],
-    #         "private": bool(row[5])
-    #     })
-
+    
     # return render_template("main.html", appointments=appointments)
